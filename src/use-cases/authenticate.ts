70d2af3b213f37579a1ac3121d44b38edcf0acd0
Jorge de Souza <jorgejrdj1@gmail.com>
2023-05-02 21:58:15 -0300

    feat: add tester and controllers

--- src/use-cases/authenticate.ts
+++ src/use-cases/authenticate.ts
@@ -4,35 +4,35 @@ import { User } from '@prisma/client'
 import { compare } from 'bcryptjs'
 
 interface AuthenticateUseCaseRequest {
-    email: string
-    password: string
+  email: string
+  password: string
 }
 
 interface AuthenticateUseCaseResponse {
-    user: User
+  user: User
 }
 
 export class AuthenticateUseCase {
-    constructor(private usersRepository: UsersRepository) { }
+  constructor(private usersRepository: UsersRepository) {}
 
-    async execute({
-        email,
-        password,
-    }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
-        const user = await this.usersRepository.findByEmail(email)
+  async execute({
+    email,
+    password,
+  }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const user = await this.usersRepository.findByEmail(email)
 
-        if (!user) {
-            throw new InvalidCredentialsError()
-        }
-
-        const doestPasswordMatches = await compare(password, user.password_hash)
-
-        if (!doestPasswordMatches) {
-            throw new InvalidCredentialsError()
-        }
-
-        return {
-            user,
-        }
+    if (!user) {
+      throw new InvalidCredentialsError()
     }
+
+    const doestPasswordMatches = await compare(password, user.password_hash)
+
+    if (!doestPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      user,
+    }
+  }
 }
