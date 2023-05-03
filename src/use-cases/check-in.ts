c87807af3ba4fd3439fde3ceba6e97baa6ea39ef
Jorge de Souza <jorgejrdj1@gmail.com>
2023-05-03 20:00:02 -0300

    feat: validate create gym

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -1,25 +1,24 @@
 import { CheckInsRepository } from '@/repositories/check-ins-repository'
 import { GymsRepository } from '@/repositories/gyms-repository'
+import { MaxDistanceError } from '@/use-cases/errors/max-distance-error'
+import { MaxNumberOfCheckInsError } from '@/use-cases/errors/max-number-of-check-ins-error'
 import { ResourceNotFoundError } from '@/use-cases/errors/resource-not-found-error'
 import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
 import { CheckIn } from '@prisma/client'
-
 interface CheckInUseCaseRequest {
   userId: string
   gymId: string
   userLatitude: number
   userLongitude: number
 }
-
 interface CheckInUseCaseResponse {
   checkIn: CheckIn
 }
-
 export class CheckInUseCase {
   constructor(
     private checkInsRepository: CheckInsRepository,
     private gymsRepository: GymsRepository,
-  ) {}
+  ) { }
 
   async execute({
     userId,
@@ -28,11 +27,9 @@ export class CheckInUseCase {
     userLongitude,
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
     const gym = await this.gymsRepository.findById(gymId)
-
     if (!gym) {
       throw new ResourceNotFoundError()
     }
-
     const distance = getDistanceBetweenCoordinates(
       { latitude: userLatitude, longitude: userLongitude },
       {
@@ -40,20 +37,21 @@ export class CheckInUseCase {
         longitude: gym.longitude.toNumber(),
       },
     )
-
     const MAX_DISTANCE_IN_KILOMETERS = 0.1
 
     if (distance > MAX_DISTANCE_IN_KILOMETERS) {
-      throw new Error()
+      throw new MaxDistanceError()
     }
 
     const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
       userId,
       new Date(),
     )
+
     if (checkInOnSameDay) {
-      throw new Error()
+      throw new MaxNumberOfCheckInsError()
     }
+
     const checkIn = await this.checkInsRepository.create({
       gym_id: gymId,
       user_id: userId,
