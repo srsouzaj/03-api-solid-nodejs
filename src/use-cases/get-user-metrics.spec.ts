8c32620c9830596164734b1013a818520f6ed9bd
Jorge de Souza <jorgejrdj1@gmail.com>
2023-05-03 21:16:10 -0300

    feat: use cases in METRICS

--- src/use-cases/get-user-metrics.spec.ts
+++ src/use-cases/get-user-metrics.spec.ts
@@ -6,26 +6,26 @@ let checkInsRepository: InMemoryCheckInsRepository
 let sut: GetUserMetricsUseCase
 
 describe('Ger User Metrics Use Case', () => {
-    beforeEach(async () => {
-        checkInsRepository = new InMemoryCheckInsRepository()
-        sut = new GetUserMetricsUseCase(checkInsRepository)
+  beforeEach(async () => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new GetUserMetricsUseCase(checkInsRepository)
+  })
+
+  it('should be able to get check-ins count from metrics', async () => {
+    await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
     })
 
-    it('should be able to get check-ins count from metrics', async () => {
-        await checkInsRepository.create({
-            gym_id: 'gym-01',
-            user_id: 'user-01',
-        })
-
-        await checkInsRepository.create({
-            gym_id: 'gym-02',
-            user_id: 'user-01',
-        })
-
-        const { checkInsCount } = await sut.execute({
-            userId: 'user-01',
-        })
-
-        expect(checkInsCount).toEqual(2)
+    await checkInsRepository.create({
+      gym_id: 'gym-02',
+      user_id: 'user-01',
     })
+
+    const { checkInsCount } = await sut.execute({
+      userId: 'user-01',
+    })
+
+    expect(checkInsCount).toEqual(2)
+  })
 })
