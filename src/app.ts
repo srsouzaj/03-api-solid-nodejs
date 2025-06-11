5d57a4dca8c2d37e7219a510c3de6b7455111dcc
Jorge de Souza <jorgedesouzajacintto_jr@hotmail.com>
2025-06-11 14:14:00 -0300

    test: refresh token

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import fastify from "fastify";
 import fastifyJwt from "@fastify/jwt";
+import fastifyCookie from "@fastify/cookie";
 import { appRoutes } from "@/http/routes";
 import { ZodError } from "zod";
 import { env } from "@/env";
@@ -12,7 +13,16 @@ app.register(appRoutes);
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "10m",
+  },
 });
+
+app.register(fastifyCookie);
 
 app.register(usersRoutes);
 app.register(gymsRoutes);
